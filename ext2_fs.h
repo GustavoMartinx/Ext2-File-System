85f109348c4fda66520d9caaef1321e232340921
Gustavo Martins <gtavomartins893@gmail.com>
2022-12-09 22:54:27 -0300

    chore: removed super-block struct

--- ext2_fs.h
+++ ext2_fs.h
@@ -145,18 +145,20 @@
    /*
     * Structure of a blocks group descriptor
     */
-   struct ext2_group_desc
-   {
-   	__u32	bg_block_bitmap;		/* Blocks bitmap block */
-   	__u32	bg_inode_bitmap;		/* Inodes bitmap block */
-   	__u32	bg_inode_table;		/* Inodes table block */
-   	__u16	bg_free_blocks_count;	/* Free blocks count */
-   	__u16	bg_free_inodes_count;	/* Free inodes count */
-   	__u16	bg_used_dirs_count;	/* Directories count */
-   	__u16	bg_pad;
-   	__u32	bg_reserved[3];
-   };
-   
+  
+     /* Structure of the group descriptor */
+/*******************************/
+     struct ext2_group_desc
+     {
+          __u32	bg_block_bitmap;		/* Blocks bitmap block */
+          __u32	bg_inode_bitmap;		/* Inodes bitmap block */
+          __u32	bg_inode_table;		/* Inodes table block */
+          __u16	bg_free_blocks_count;	/* Free blocks count */
+          __u16	bg_free_inodes_count;	/* Free inodes count */
+          __u16	bg_used_dirs_count;	/* Directories count */
+          __u16	bg_pad;
+          __u32	bg_reserved[3];
+     };
    /*
     * Macro-instructions used to manage group descriptors
     */
@@ -333,67 +335,7 @@
    #define EXT2_ERRORS_PANIC		3	/* Panic */
    #define EXT2_ERRORS_DEFAULT		EXT2_ERRORS_CONTINUE
    
-   /*
-    * Structure of the super block
-    */
-   struct ext2_super_block {
-   	__u32	s_inodes_count;		/* Inodes count */
-   	__u32	s_blocks_count;		/* Blocks count */
-   	__u32	s_r_blocks_count;	/* Reserved blocks count */
-   	__u32	s_free_blocks_count;	/* Free blocks count */
-   	__u32	s_free_inodes_count;	/* Free inodes count */
-   	__u32	s_first_data_block;	/* First Data Block */
-   	__u32	s_log_block_size;	/* Block size */
-   	__s32	s_log_frag_size;	/* Fragment size */
-   	__u32	s_blocks_per_group;	/* # Blocks per group */
-   	__u32	s_frags_per_group;	/* # Fragments per group */
-   	__u32	s_inodes_per_group;	/* # Inodes per group */
-   	__u32	s_mtime;		/* Mount time */
-   	__u32	s_wtime;		/* Write time */
-   	__u16	s_mnt_count;		/* Mount count */
-   	__s16	s_max_mnt_count;	/* Maximal mount count */
-   	__u16	s_magic;		/* Magic signature */
-   	__u16	s_state;		/* File system state */
-   	__u16	s_errors;		/* Behaviour when detecting errors */
-   	__u16	s_minor_rev_level; 	/* minor revision level */
-   	__u32	s_lastcheck;		/* time of last check */
-   	__u32	s_checkinterval;	/* max. time between checks */
-   	__u32	s_creator_os;		/* OS */
-   	__u32	s_rev_level;		/* Revision level */
-   	__u16	s_def_resuid;		/* Default uid for reserved blocks */
-   	__u16	s_def_resgid;		/* Default gid for reserved blocks */
-   	/*
-   	 * These fields are for EXT2_DYNAMIC_REV superblocks only.
-   	 *
-   	 * Note: the difference between the compatible feature set and
-   	 * the incompatible feature set is that if there is a bit set
-   	 * in the incompatible feature set that the kernel doesn't
-   	 * know about, it should refuse to mount the filesystem.
-   	 * 
-   	 * e2fsck's requirements are more strict; if it doesn't know
-   	 * about a feature in either the compatible or incompatible
-   	 * feature set, it must abort and not try to meddle with
-   	 * things it doesn't understand...
-   	 */
-   	__u32	s_first_ino; 		/* First non-reserved inode */
-   	__u16   s_inode_size; 		/* size of inode structure */
-   	__u16	s_block_group_nr; 	/* block group # of this superblock */
-   	__u32	s_feature_compat; 	/* compatible feature set */
-   	__u32	s_feature_incompat; 	/* incompatible feature set */
-   	__u32	s_feature_ro_compat; 	/* readonly-compatible feature set */
-   	__u8	s_uuid[16];		/* 128-bit uuid for volume */
-   	char	s_volume_name[16]; 	/* volume name */
-   	char	s_last_mounted[64]; 	/* directory where last mounted */
-   	__u32	s_algorithm_usage_bitmap; /* For compression */
-   	/*
-   	 * Performance hints.  Directory preallocation should only
-   	 * happen if the EXT2_COMPAT_PREALLOC flag is on.
-   	 */
-   	__u8	s_prealloc_blocks;	/* Nr of blocks to try to preallocate*/
-   	__u8	s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
-   	__u16	s_padding1;
-   	__u32	s_reserved[204];	/* Padding to the end of the block */
-   };
+   
    
    #ifdef __KERNEL__
    #define EXT2_SB(sb)	(&((sb)->u.ext2_sb))
@@ -502,7 +444,7 @@
    	__u32	inode;			/* Inode number */
    	__u16	rec_len;		/* Directory entry length */
    	__u8	name_len;		/* Name length */
-   	__u8	file_type;
+   	char	file_type;
    	char	name[EXT2_NAME_LEN];	/* File name */
    };
    
