5975130dcd14b474db2c16e429938b797bfb4681
Gustavo Martins <gtavomartins893@gmail.com>
2022-12-10 20:14:17 -0300

    feat: create change ditectory func

--- ext2.c
+++ ext2.c
@@ -332,6 +332,7 @@ void change_directory(char* dirName, struct ext2_inode *inode, struct ext2_group
 	else if(nomeArquivo == ".."){
 		POP(&orig);
 	} 
+	*/
 
 /*cd livros
 livros
@@ -339,6 +340,72 @@ inode: 8193
 record lenght: 16
 name lenght: 6
 file type: 2*/
+
+
+	void *block;
+
+	if (S_ISDIR(inode->i_mode)) {
+		struct ext2_dir_entry_2 *entry;
+		unsigned int size = 0;
+
+		if ((block = malloc(block_size)) == NULL) { /* allocate memory for the data block */
+			fprintf(stderr, "Memory error\n");
+			close(fd);
+			exit(1);
+		}
+
+		lseek(fd, BLOCK_OFFSET(inode->i_block[0]), SEEK_SET);
+		read(fd, block, block_size);                /* read block from disk*/
+
+		entry = (struct ext2_dir_entry_2 *) block;  /* first entry in the directory */
+
+		while((size < inode->i_size) && entry->inode) {
+			char file_name[EXT2_NAME_LEN+1];
+			memcpy(file_name, entry->name, entry->name_len);
+			file_name[entry->name_len] = 0;     /* append null character to the file name */
+
+			// PARA RETORNAR INODE
+			if((strcmp(dirName, entry->name)) == 0){
+				
+				//parametros do cd 
+				printf("%s\n"
+				       "inode: %10u\n"
+					   "Record lenght: %hu\n"
+				       "Name lenght: %d\n"
+					   "File type: %d\n\n",
+						file_name,
+						entry->inode,
+						entry->rec_len,
+						entry->name_len,
+						entry->file_type);
+				break;
+			}
+
+			entry = (void*) entry + entry->rec_len;
+			size += entry->rec_len;
+		}
+
+		free(block);
+	}
+	
+	printf("\n\n");
+
+
+
+	read_inode(fd, 2, group, inode);
+
+	unsigned int dirNameInode = read_dir(fd, inode, group, dirName);
+
+	chage_group(&dirNameInode, group, currentGroup);
+
+	unsigned int index = ((int)dirNameInode) % super.s_inodes_per_group;
+
+	read_inode(fd, index, group, inode);
+
+	ls(inode, group);
+	
+	
+}
 
 
 
